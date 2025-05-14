b8751fc57dc65d2b2e43721971734043a49bf6c9
Ravi Tiwari <rtiwari6@asu.edu>
2025-05-13 19:24:12 -0700

    Config Firebase

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,7 +14,7 @@ async function Home() {
   const user = await getCurrentUser();
 
   const [userInterviews, allInterview] = await Promise.all([
-    getInterviewsByUserId(user?.id!),
+    getInterviewsByUserId(user?.id),
     getLatestInterviews({ userId: user?.id! }),
   ]);
 
